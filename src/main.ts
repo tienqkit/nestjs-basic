83de7dfe98f0800f0fbcaefea0307ab4e33758ff
Tien Nguyen <tien.nguyen@qkit.vn>
2025-05-14 23:30:27 +0700

    Enhance ValidationPipe to customize error responses with BadRequestException

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { ValidationError } from 'class-validator';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new BadRequestException(
+          validationErrors.map((error) => ({
+            [error.property]: Object.values(error.constraints ?? {})[0],
+          })),
+        );
+      },
+    }),
+  );
   await app.listen(port);
 }
 bootstrap();
